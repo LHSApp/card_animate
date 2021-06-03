ebbc14aa54408ab6bf309b2a8b03b8fc161b13c9
Leandro Santos <leandrosantos020788@gmail.com>
2021-06-02 22:49:52 -0300

    Update

--- script/variaveis/script.js
+++ script/variaveis/script.js
@@ -1,7 +1,12 @@
-var nome = 'Leandro';
-var idade = 32;
-var comida;
-comida = 'lanche';
+var nome = 'Leandro'; // string
+var idade = 32; // number
+var possuuFaculdade = true; //boolean
+var comida; // undefined
+var comida = null; //null
+var simbolo = Symbol(); //Symbol
+var novoObjeto = {} // objeto
+
+
 var Leandro, Lilian, Maite, Aria, Pandora;
 
 console.log(nome, idade, comida);
