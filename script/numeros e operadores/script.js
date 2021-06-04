1806395268e55f27bcbe6a620a23b106905dcb4c
Leandro Santos <leandrosantos020788@gmail.com>
2021-06-04 10:55:19 -0300

    Update

---
+++ script/numeros e operadores/script.js
@@ -0,0 +1,37 @@
+//NÚMEROS
+var idade = 28;
+var gols = 1000;
+var pi = 3.14; // ponto para decimal
+var exp = 2e10; // 20000000000
+
+
+console.log(idade);
+
+//OPERADORES ARITMÉTICOS
+var soma = 100 + 50; // 150
+var subtracao = 100 - 50; // 50
+var multiplicacao = 100 * 2; // 200
+var divisao = 100 / 2; // 50
+var expoente = 2 ** 4; // 16
+var modulo = 14 % 5; // 4
+
+console.log(soma);
+
+//OPERADORES ARITMÉTICOS (STRINGS)
+
+var soma = '100' + 50; // 10050
+var subtracao = '100' - 50; // 50
+var multiplicacao = '100' * '2'; // 200
+var divisao = 'Comprei 10' / 2; // NaN (Not a Number)
+
+console.log(multiplicacao)
+
+//NAN = NOT A NUMBER
+
+var numero = 80;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+var pesoPorDois = peso / 2 // NaN (Not a Number)
+
+
+console.log(peso)
