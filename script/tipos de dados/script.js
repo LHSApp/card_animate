eca5b8cfc85275ede7f947aad7d91e4722502c94
Leandro Santos <leandrosantos020788@gmail.com>
2021-06-03 21:55:11 -0300

    Update

---
+++ script/tipos de dados/script.js
@@ -0,0 +1,12 @@
+var nome = 'Leandro';
+var idade = 28;
+var simbolo = Symbol();
+
+console.log(typeof simbolo);
+
+
+var nome = 'Leandro';
+var sobrenome = 'Santos';
+var nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto)
