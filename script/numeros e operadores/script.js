5322c76d877cddf6572fa21930ec35fbc3ca2a93
Leandro Santos <leandrosantos020788@gmail.com>
2021-06-09 14:11:55 -0300

    Update

--- script/numeros e operadores/script.js
+++ script/numeros e operadores/script.js
@@ -76,16 +76,27 @@ console.log(+possuiFaculdade); // 1
 var total = 10 + 5 * 2 / 2 + 20;
 
 // Crie duas expressões que retornem NaN
-var texto = 'Exercicio' + frase
-    // Somar a string '200' com o número 50 e retornar 250
+var texto = 'Exercicio' / 3
+console.log(texto)
+
+
+
+// Somar a string '200' com o número 50 e retornar 250
 var idade = +'200';
 var somaIdade = 50;
-
 console.log(idade + somaIdade)
-    // Incremente o número 5 e retorne o seu valor incrementado
+
+
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var X = 5;
+console.log(++X);
+
+
+
 
 // Como dividir o peso por 2?
-var numero = '80';
+var numero = +'80' / 2;
 var unidade = 'kg';
 var peso = numero + unidade; // '80kg'
-var pesoPorDois = peso / 2; // NaN (Not a Number)
+console.log(peso)
