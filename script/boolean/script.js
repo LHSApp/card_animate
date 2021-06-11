8acfc6ed8fd4d88b02ca7391a39b8ac00af4d115
Leandro Santos <leandrosantos020788@gmail.com>
2021-06-10 22:42:27 -0300

    Update

--- script/boolean/script.js
+++ script/boolean/script.js
@@ -6,3 +6,58 @@ if (possuiGraduacao) {
     console.log('Não possui graduação');
 }
 // retorna Possui Graduação e não executa o else
+
+var possuiGraduacao = true;
+var possuiDoutorado = false;
+
+if (possuiDoutorado) {
+    console.log('Possui graduação e doutorado');
+} else if (possuiGraduacao) {
+    console.log('Possui graduação, mas não possui doutorado');
+} else {
+    console.log('Não possui graduação');
+}
+// retorna Possui Graduação, mas não possui doutorado
+
+// Falsy
+if (false)
+    if (0) // ou -0
+        if (NaN)
+        if (null)
+            if (undefined)
+                if ('') // ou "" ou ``
+
+
+                // Truthy
+                    if (true)
+                    if (1)
+                        if (' ')
+                            if ('andre')
+                                if (-5)
+                                    if ({});
+
+
+                                    // Truthy
+if (!true) // false
+    if (!1) // false
+        if (!'') // true
+            if (!undefined) // true
+                if (!!' ') // true
+                    if (!!''); // false
+
+
+
+10 > 5; // true
+5 > 10; // false
+20 < 10; // false
+10 <= 10 // true
+10 >= 11 // false
+
+
+10 == '10'; // true
+10 == 10; // true
+10 === '10'; // false
+10 === 10 // true
+10 != 15 // true
+10 != '10' // false
+10 !== '10' // true
