fd5064a71a58f2c76154d7a17615ca315ae50714
Leandro Santos <leandrosantos020788@gmail.com>
2021-06-29 11:14:08 -0300

    Update

--- script/Tudo e objeto/script.js
+++ script/Tudo e objeto/script.js
@@ -6,3 +6,22 @@ btn.innerText; // 'Clique'
 btn.addEventListener('click', function() {
     console.log('Clicou')
 })
+
+
+// nomeie 3 propriedades ou métodos de strings
+var nome = 'Leandro'
+    //small
+    //strike
+    //fixed
+    // nomeie 5 propriedades ou métodos de elementos do DOM
+
+var btn = document.querySelector('.btn');
+
+//addEventListener
+//appendChild
+// id 
+//innerHtml
+console.log(btn)
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
