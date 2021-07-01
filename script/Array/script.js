f32aefa5fa316bc5e922a52dde08d6dc525e73e3
Leandro Santos <leandrosantos020788@gmail.com>
2021-06-30 23:01:47 -0300

    Update

---
+++ script/Array/script.js
@@ -0,0 +1,20 @@
+var videoGames = ['PS1', 'PS2', 'PS3'];
+
+var ultimoItem = videoGames.pop();
+
+videoGames.push('3DS');
+
+
+
+//FOR LOOP
+
+for (var numero = 1; numero <= 10; numero++) {
+    console.log(numero);
+}
+
+
+var i = 0;
+while (i <= 100) {
+    console.log(i);
+    i++
+}
--- script/Tudo e objeto/script.js
+++ script/Tudo e objeto/script.js
@@ -25,3 +25,5 @@ console.log(btn)
 
 // busque na web um objeto (método) capaz de interagir com o clipboard, 
 // clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+// https: //clipboardjs.com/
