b0df403f15575c6c65e055fa5b790591a3fa9742
Leandro Santos <leandrosantos020788@gmail.com>
2021-07-07 22:34:45 -0300

    Update

--- script/Atribuição e Ternário/script.js
+++ script/Atribuição e Ternário/script.js
@@ -40,4 +40,77 @@ var possuiCasa = true;
 var darCredito;
 
 darCredito = (possuiCarro && possuiCasa);
+darCredito = (possuiCarro && possuiCasa) ? 'Dar crédito ao cliente' : 'Negar crédito';
 console.log(darCredito);
+
+
+// Escopo de Função
+
+
+// Por qual motivo o código abaixo retorna com erros?
+//{
+//var cor = 'preto';
+//  const marca = 'Fiat';
+// let portas = 4;
+//}
+//console.log(var, marca, portas);
+
+//resposta
+
+{
+    var cor = 'preto';
+    const marca = 'Fiat';
+    let portas = 4;
+    console.log(cor, marca, portas);
+}
+
+
+
+// Como corrigir o erro abaixo?
+//function somarDois(x) {
+// const dois = 2;
+//return x + dois;
+//}
+
+//function dividirDois(x) {
+//return x + dois;
+//}
+//somarDois(4);
+//dividirDois(6);
+// Resposta
+const dois = 2;
+
+function somarDois(x) {
+
+    return x + dois;
+}
+
+function dividirDois(x) {
+    return x + dois;
+}
+console.log(somarDois(4));
+console.log(dividirDois(10));
+
+
+
+// O que fazer para total retornar 500?
+//var numero = 50;
+
+//for (var numero = 0; numero < 10; numero++) {
+// console.log(numero);
+//}
+
+//const total = 10 * numero;
+//console.log(total);
+
+
+//Resposta
+
+var numero = 50;
+
+for (let numero = 0; numero < 10; numero++) {
+    console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
