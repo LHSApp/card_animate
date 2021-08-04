5c5d2711c58a9ed0e563c248f9683d482dbd9b70
Leandro Santos <leandrosantos020788@gmail.com>
2021-08-03 21:22:02 -0300

    Update

--- script/Classes e Atributos/Project 1/script/script.js
+++ script/Classes e Atributos/Project 1/script/script.js
@@ -28,3 +28,36 @@ console.log(animais.attributes.id);
 const img = document.querySelector('img');
 
 console.log(img.getAttribute('src'));
+
+
+//Exercico
+
+// Adicione a classe ativo a todos os itens do menu
+const intemMenu = document.querySelectorAll('.menu a');
+intemMenu.forEach((item) => {
+        item.classList.add('ativo');
+
+    })
+    // Remove a classe ativo de todos os itens do menu e mantenha apenas no primeiro
+
+intemMenu.forEach((item) => {
+    item.classList.remove('ativo');
+
+});
+
+intemMenu[0].classList.add('ativo');
+
+// Verifique se as imagens possuem o atributo alt
+const imgs = document.querySelectorAll('img');
+
+imgs.forEach((img) => {
+
+    const possuiAtributo = img.hasAttribute('alt');
+    console.log(img, possuiAtributo);
+});
+// Modifique o href do link externo no menu
+
+const link = document.querySelector('a[href^="http"]');
+
+link.setAttribute('href', 'https://www.google.com/')
+console.log(link)
