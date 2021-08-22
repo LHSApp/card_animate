790eb49ecd578c5cd27d2c8158804b6550fde056
Leandro Santos <leandrosantos020788@gmail.com>
2021-08-22 18:20:52 -0300

    Update

--- script/Navegação por Tabs/script/evento1.js
+++ script/Navegação por Tabs/script/evento1.js
@@ -26,3 +26,27 @@ function initTabNav() {
 
     }
 }
+initTabNav();
+
+
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+
+    const activeCalss = 'ativo'
+    if (accordionList.length) {
+        accordionList[0].classList.add(activeCalss);
+        accordionList[0].nextElementSibling.classList.add(activeCalss);
+
+        function activeAccordion() {
+            this.classList.toggle(activeCalss);
+
+            this.nextElementSibling.classList.toggle(activeCalss);
+        }
+        accordionList.forEach((item) => {
+
+            item.addEventListener('click', activeAccordion);
+        });
+    }
+}
+
+initAccordion();
