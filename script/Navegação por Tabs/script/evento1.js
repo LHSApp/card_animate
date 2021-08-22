f84dd16177eefc286997c8458b14bd3ff43bd30b
Leandro Santos <leandrosantos020788@gmail.com>
2021-08-22 17:56:15 -0300

    Update

--- script/Navegação por Tabs/script/evento1.js
+++ script/Navegação por Tabs/script/evento1.js
@@ -1,26 +1,28 @@
-const tabMenu = document.querySelectorAll('.js-tabmenu li');
+function initTabNav() {
+    const tabMenu = document.querySelectorAll('.js-tabmenu li');
 
-const tabContent = document.querySelectorAll('.js-tabcontent section');
+    const tabContent = document.querySelectorAll('.js-tabcontent section');
 
 
-if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add('ativo');
+    if (tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add('ativo');
 
-    function activeTab(index) {
+        function activeTab(index) {
 
-        tabContent.forEach((section) => {
+            tabContent.forEach((section) => {
 
-            section.classList.remove('ativo')
-        })
+                section.classList.remove('ativo')
+            })
 
-        tabContent[index].classList.add('ativo');
-    }
+            tabContent[index].classList.add('ativo');
+        }
 
-    tabMenu.forEach((itemMenu, index) => {
+        tabMenu.forEach((itemMenu, index) => {
 
-        itemMenu.addEventListener('click', function() {
-            activeTab(index);
+            itemMenu.addEventListener('click', function() {
+                activeTab(index);
+            });
         });
-    });
 
+    }
 }
